52c67c60ebda286a0b23aea492f4b0b7781e858f
Heisenburger69 <apkforgss@gmail.com>
2020-05-04 20:59:43 +0530

    Implement #6

--- src/Heisenburger69/BurgerCustomArmor/Abilities/AbilityUtils.php
+++ src/Heisenburger69/BurgerCustomArmor/Abilities/AbilityUtils.php
@@ -32,33 +32,33 @@ class AbilityUtils
     {
         switch ($ability) {
             case "Scale":
-                return new ScaleAbility($value);
+                return new ScaleAbility((float)$value);
             case "Permission":
-                return new PermissionAbility($value);
+                return new PermissionAbility((string)$value);
             case "Knockback":
-                return new KnockbackNegationAbility($value);
+                return new KnockbackNegationAbility((float)$value);
             case "DamageNegation":
-                return new DamageNegationAbility($value);
+                return new DamageNegationAbility((float)$value);
             case "SwordNegation":
-                return new SwordNegationAbility($value);
+                return new SwordNegationAbility((float)$value);
             case "AxeNegation":
-                return new AxeNegationAbility($value);
+                return new AxeNegationAbility((float)$value);
             case "BowNegation":
-                return new BowNegationAbility($value);
+                return new BowNegationAbility((float)$value);
             case "DamageAmplification":
-                return new DamageAmplificationAbility($value);
+                return new DamageAmplificationAbility((float)$value);
             case "SwordAmplification":
-                return new SwordAmplificationAbility($value);
+                return new SwordAmplificationAbility((float)$value);
             case "AxeAmplification":
-                return new AxeAmplificationAbility($value);
+                return new AxeAmplificationAbility((float)$value);
             case "BowAmplification":
-                return new BowAmplificationAbility($value);
+                return new BowAmplificationAbility((float)$value);
             case "Cape":
                 if (!extension_loaded("gd")) {
                     Server::getInstance()->getLogger()->info(Main::PREFIX . ": gd extension missing! Cape Ability Disabled.");
                     return null;
                 }
-                return new CapeAbility($value);
+                return new CapeAbility((string)$value);
             default:
                 return null;
         }
@@ -77,7 +77,7 @@ class AbilityUtils
         }
         foreach ($value as $effect) {
             foreach ($effect as $effectName => $level) {
-                $effectInstance = new EffectInstance(Effect::getEffectByName($effectName), 999999, $level - 1);
+                $effectInstance = new EffectInstance(Effect::getEffectByName($effectName), 999999, $level - 1, Main::$instance->getConfig()->get("show-effect-particles"));
                 $abilities[] = new EffectAbility($effectInstance);
             }
         }
